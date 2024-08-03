b1d4eb2206e6ee1fe8302e9e71d5d4c38eb67102
publisher-V <dbxor1568@naver.com>
2024-08-03 23:39:51 +0900

    명언 생성기 구현 완료

--- quote-generator/js/script.js
+++ quote-generator/js/script.js
@@ -3,10 +3,26 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+//로딩 중
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//로딩 완료
+function complete() {
+  setTimeout(() => {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+  }, 500);
+}
+
 function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   console.log(quote);
 
@@ -23,10 +39,12 @@ function newQuote() {
     authorText.style.fontSize = "1.7rem";
   }
   quoteText.textContent = quote.q;
+  complete();
 }
 
 // 인용문 API 가져오기
 async function getQuotes() {
+  loading();
   const apiUrl = "https://zenquotes.io/api/quotes";
   try {
     const response = await fetch(apiUrl);
